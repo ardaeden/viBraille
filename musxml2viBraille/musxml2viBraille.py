297ef3857a3eece65a6cf710596e4821c9ea1146
Arda Eden <ardaeden@gmail.com>
2019-02-19 10:04:32 +0300

    measures added

--- musxml2viBraille/musxml2viBraille.py
+++ musxml2viBraille/musxml2viBraille.py
@@ -40,79 +40,82 @@ text_file = open("output.txt","w")
 if (text_file):
     print('File opened without any problem ...\n')
 
-for target in root.findall('./part/measure/note'):
-    
-    noteType = noteTypes.get(target.find('type').text)
 
-    # If note is a rest, use "r" as the symbol
-    if (target.find('rest') is not None):
-        print('r' + noteType)
-        text_file.write('r' + noteType + '\n')
+for measures in root.findall('./part/measure'):
+
+    for notes in measures.findall('note'):
         
-    #if not a rest keep on ...
-    else:
-        
-        noteStep = noteSteps.get(target.find('pitch/step').text)
-        noteOct = int(target.find('pitch/octave').text)
+        noteType = noteTypes.get(notes.find('type').text)
+
+        # If note is a rest, use "r" as the symbol
+        if (notes.find('rest') is not None):
+            print('r' + noteType)
+            text_file.write('r' + noteType + '\n')
+            
+        #if not a rest keep on ...
+        else:
+            
+            noteStep = noteSteps.get(notes.find('pitch/step').text)
+            noteOct = int(notes.find('pitch/octave').text)
 
 
-        # !!! Note interval and octave code decision is done here !!!
-        # -----------------------------------------------------------
-        
-        # Create an octave code for the first note
-        if (isFirstNote is True):
-            oldOct = noteOct
-            oldIndex = stepIndexes.get(noteStep)
-            print('o' + str(noteOct))
-            text_file.write('o' + str(noteOct) + '\n')
-            isFirstNote = False
-
-        # Do the math and decide for octave codes :))
-        else: 
-            idx = stepIndexes.get(noteStep)
-            o = noteOct - oldOct
-            interval = abs((o * 7) + (idx - oldIndex)) + 1
-            if ((interval >= 6) or ((interval in range(4,6)) and o != 0)):
+            # !!! Note interval and octave code decision is done here !!!
+            # -----------------------------------------------------------
+            
+            # Create an octave code for the first note
+            if (isFirstNote is True):
+                oldOct = noteOct
+                oldIndex = stepIndexes.get(noteStep)
                 print('o' + str(noteOct))
                 text_file.write('o' + str(noteOct) + '\n')
-            oldIndex = idx
-            oldOct = noteOct
+                isFirstNote = False
 
-        #Get accidentals if exist.
-        if (target.find('accidental')) is not None:
-            acc = target.find('accidental').text
-            
-            if (acc=='flat-flat'):
-                for i in range(2):    
+            # Do the math and decide for octave codes :))
+            else: 
+                idx = stepIndexes.get(noteStep)
+                o = noteOct - oldOct
+                interval = abs((o * 7) + (idx - oldIndex)) + 1
+                if ((interval >= 6) or ((interval in range(4,6)) and o != 0)):
+                    print('o' + str(noteOct))
+                    text_file.write('o' + str(noteOct) + '\n')
+                oldIndex = idx
+                oldOct = noteOct
+
+            #Get accidentals if exist.
+            if (notes.find('accidental')) is not None:
+                acc = notes.find('accidental').text
+                
+                if (acc=='flat-flat'):
+                    for i in range(2):    
+                        print('-')
+                        text_file.write('-' + '\n')
+                
+                if (acc=='flat'):
                     print('-')
                     text_file.write('-' + '\n')
-            
-            if (acc=='flat'):
-                print('-')
-                text_file.write('-' + '\n')
 
-            if (acc=='sharp'):
-                print('+')
-                text_file.write('+' + '\n')
-
-            if (acc=='double-sharp'):
-                for i in range(2):    
+                if (acc=='sharp'):
                     print('+')
                     text_file.write('+' + '\n')
 
-            if (acc=='natural'):
-                print('n')
-                text_file.write('n' + '\n')
+                if (acc=='double-sharp'):
+                    for i in range(2):    
+                        print('+')
+                        text_file.write('+' + '\n')
 
-        # Create a note code in output
-        print(noteStep + noteType)
-        text_file.write(noteStep + noteType + '\n')
+                if (acc=='natural'):
+                    print('n')
+                    text_file.write('n' + '\n')
 
-    # Get the dot count and create a dot
-    # code in output for each <dot/> tag in note
-    notedotcount = len(target.findall('dot'))
-    for i in range(notedotcount):
-        print('.')
-        text_file.write('.' + '\n')
+            # Create a note code in output
+            print(noteStep + noteType)
+            text_file.write(noteStep + noteType + '\n')
+
+        # Get the dot count and create a dot
+        # code in output for each <dot/> tag in note
+        notedotcount = len(notes.findall('dot'))
+        for i in range(notedotcount):
+            print('.')
+            text_file.write('.' + '\n')
 
 text_file.close()
